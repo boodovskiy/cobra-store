cdc73fa860ff3dd1f98047334353e14dc2cdeb37
boodovskiy <dzak1987@mail.ru>
2024-11-05 10:04:45 +0200

    Add ReviewColumn component

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -1,8 +1,9 @@
 "use client"
 
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import MaxWidthWrapper from './MaxWidthWrapper'
 import { useInView } from 'framer-motion'
+import { cn } from '@/lib/utils'
 
 const PHONES = [
     "/testimonials/1.jpg",
@@ -29,6 +30,46 @@ function splitArray<T>(array: Array<T>, numParts: number){
     return result
 }
 
+function ReviewColumn({
+    reviews,
+    className,
+    reviewClassName,
+    msPerPixel = 0
+} : {
+    reviews: string[]
+    className?: string
+    reviewClassName?: (reviewIndex: number) => string
+    msPerPixel?: number
+}){
+    const columnRef = useRef<HTMLDivElement | null>(null)
+    const [columnHeight, setColumnHeight] = useState(0)
+    const duration = `${columnHeight * msPerPixel}ms`
+
+    useEffect(() => {
+        if(!columnRef.current) return
+
+        const resizeObserver = new window.ResizeObserver(() => {
+            setColumnHeight(columnRef.current?.offsetHeight ?? 0)
+        })
+
+        resizeObserver.observe(columnRef.current)
+
+        return () => {
+            resizeObserver.disconnect()
+        }
+    }, [])
+
+    return <div 
+        ref={columnRef} 
+        className={cn('animate-marquee space-y-8 py-4', className)}
+        style={{'--marquee-duration': duration} as React.CSSProperties}
+        >
+            {/* {reviews.concat(reviews).map((imgSrc, reviewIndex)=>{
+                <Review />
+            })} */}
+    </div>
+}
+
 function ReviewGrid() {
     const containerRef = useRef<HTMLDivElement | null>(null)
     const isInView = useInView(containerRef, {once: true, amount: 0.4})
@@ -41,7 +82,7 @@ function ReviewGrid() {
      ref={containerRef} 
      className='relative -mx-4 mt-16 grid h-[49rem] max-h-[150vh] grid-cols-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3'>
         {isInView ? <>
-            {/* <ReviewColumn /> */}
+            <ReviewColumn />
         </> : null}
     </div>
 }
