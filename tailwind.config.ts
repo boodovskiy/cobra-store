0e48aae5267db6cc5515107c68111aa6de586931
boodovskiy <dzak1987@mail.ru>
2024-11-06 11:33:02 +0200

    Add Review component and ReviewGrid animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -61,10 +61,19 @@ const config: Config = {
 				'100%': {
 					transform: 'translateY(-50%)'
 				}
+			},
+			"fade-in": {
+				from: {
+					opacity: "0"
+				},
+				to: {
+					opacity: "1"
+				},
 			}
 		},
 		animation: {
 			"marquee": 'marquee var(--marquee-duration) linear infinite',
+			"fade-in": 'fade-in 0.5s linear forwards'
 		}
   	}
   },
