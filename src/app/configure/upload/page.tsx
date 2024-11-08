ca52907bbd47b01ccd50cc92f5c4fc77b11d37ce
boodovskiy <dzak1987@mail.ru>
2024-11-08 18:03:14 +0200

    Add Configure/Upload Page, install React Dropzone

--- src/app/configure/upload/page.tsx
+++ src/app/configure/upload/page.tsx
@@ -1,64 +1,14 @@
 'use client'
 
-import { Progress } from '@/components/ui/progress'
-import { useToast } from '@/hooks/use-toast'
-import { useUploadThing } from '@/lib/uploadthing'
 import { cn } from '@/lib/utils'
-import { Image, Loader2Icon, MousePointerSquareDashed } from 'lucide-react'
-import { useRouter } from 'next/navigation'
-import { useState, useTransition } from 'react'
+import React, { useState } from 'react'
 import Dropzone, { FileRejection } from 'react-dropzone'
 
 const Page = () => {
-    const { toast } = useToast()
     const [isDragOver, setIsDragOver] = useState<boolean>(false)
-    const [uploadProgress, setUploadProgress] = useState<number>(0)
-    const router = useRouter()
 
-    const { startUpload, isUploading } = useUploadThing("imageUploader", {
-        onClientUploadComplete: (data) => {
-            console.log("Upload data received:", data); // Inspect the received data
-            let configId: string | undefined;            
-            
-            // Find the first configId from serverData in the array
-            const fileWithConfig = data.find((file) => file.serverData?.configId);
-
-            if (fileWithConfig) {
-                configId = fileWithConfig.serverData.configId;
-                
-                // Perform the transition after setting configId
-                startTransition(() => {
-                    router.push(`/configure/design?id=${configId}`)
-                })
-            } else {
-                console.error("No configId found in the uploaded files.");
-            }
-        },
-        onUploadProgress(p) {
-           setUploadProgress(p) 
-        },
-    })
-
-    const onDropRejected = (rejectedFiles: FileRejection[]) => {
-        const [file] = rejectedFiles
-
-        setIsDragOver(false)
-        toast({
-            title: `${file.file.type} type is not supported.`,
-            description: "Please choose PNG, JPEG or JPG image instead.",
-            variant: "destructive"
-        })
-    }
-
-    const onDropAccepted = (acceptedFiles: File[]) => { 
-        startUpload(acceptedFiles, { configId: undefined })
-            .catch((error) => {
-                console.error("Error during upload:", error);
-            });
-        setIsDragOver(false)
-    }
-
-    const [isPending, startTransition] = useTransition()
+    const onDropRejected = () => {}
+    const onDropAccepted = () => { console.log("accepted") }
 
   return (
     <div className={
@@ -79,41 +29,8 @@ const Page = () => {
                 onDragLeave={ () => setIsDragOver(true)}
             >
                 {({getRootProps, getInputProps}) => (
-                    <div className="h-full w-full flex flex-1 flex-col items-center justify-center" {...getRootProps()}>
-                        <input {...getInputProps()} />
-                        {isDragOver ? (
-                            <MousePointerSquareDashed className='h-6 w-6 text-zinc-500 mb-2' /> 
-                        )   : isUploading || isPending ? ( 
-                            <Loader2Icon className='animate-spin h-6 w-6  text-zinc-500 mb-2'/>
-                        )   : (
-                            <Image className='h-6 w-6 text-zinc-500 mb-2' />
-                        )}
-                        <div className="flex flex-col justify-center mb-2 text-sm text-zinc-700">
-                            {isUploading ? (
-                                <div className='flex flex-col items-center'>
-                                    <p>Uploading...</p>
-                                    <Progress 
-                                        value={uploadProgress}
-                                        className='mt-2 w-40 h-2 bg-gray-300' 
-                                    />
-                                </div> 
-                            )   : isPending ? ( 
-                                <div className='flex flex-col items-center'>
-                                    <p>Redireting, please wait...</p>
-                                </div> 
-                            )   : isDragOver ? (
-                                <p>
-                                    <span className='font-semibold'>Drop file</span> to upload
-                                </p>
-                            )   : (
-                                <p>
-                                    <span className='font-semibold'>Click to upload</span> or drag and drop
-                                </p>
-                            )}
-                        </div>
-                        {isPending ? null : (
-                            <p className='text-xs text-zinc-500'>PNG, JPEG, JPG</p>
-                        )}
+                    <div className="h-full w-full flex-1 flex-col items-center justify-center" {...getRootProps()}>
+                        <input {...getInputProps()} />1
                     </div>
                 )}
             </Dropzone>
