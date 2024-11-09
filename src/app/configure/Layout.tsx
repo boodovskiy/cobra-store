ab9e4d7660da8a9de2c087ef938c6ead33e66091
boodovskiy <dzak1987@mail.ru>
2024-11-09 11:14:50 +0200

    Add Layout wrapper for Configure

--- src/app/configure/Layout.tsx
+++ src/app/configure/Layout.tsx
@@ -1,11 +1,9 @@
 import MaxWidthWrapper from '@/components/MaxWidthWrapper'
-import Steps from '@/components/Steps'
-import { ReactNode } from 'react'
+import React, { ReactNode } from 'react'
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <MaxWidthWrapper className='flex-1 flex flex-col'>
-        <Steps />
         {children}
     </MaxWidthWrapper>
   )
