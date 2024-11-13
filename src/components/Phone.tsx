700d04b1cadb3e394fc5760aee254fbef6d79d35
boodovskiy <dzak1987@mail.ru>
2024-11-13 10:54:04 +0200

    Add markup for configure page (steps), minor fixes

--- src/components/Phone.tsx
+++ src/components/Phone.tsx
@@ -8,7 +8,7 @@ interface PhoneProps extends HTMLAttributes<HTMLDivElement> {
 
 const Phone = ({ imgSrc, className, dark = false, ...props }: PhoneProps) => {
   return (
-    <div className={cn("relative pointer-events-none z-50 overflow-hidden, className")} {...props}>
+    <div className={cn("relative pointer-events-none z-50 overflow-hidden", className)} {...props}>
         <img src={dark ? "/phone-template-dark-edges.png" : "/phone-template-white-edges.png" } 
             className='pointer-events-none z-50 select-none'
             alt='phone image'
