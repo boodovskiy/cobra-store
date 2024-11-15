325647a03bdcbd9387fdce7d12144fdb73a5aaf7
boodovskiy <dzak1987@mail.ru>
2024-11-15 15:11:41 +0200

    Retrive configId for Configurator Design page

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,8 +1,32 @@
+import { db } from '@/db'
+import { notFound } from 'next/navigation'
 import React from 'react'
 
-const Page = async () => {
+interface PageProps {
+    searchParams: {
+        [key: string]: string | string[] | undefined
+    }
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+    const { id } = searchParams
+    
+    if (!id || typeof id !== "string"){
+        return notFound()
+    }
+
+    const configuration = await db.configuration.findUnique({
+        where: { id },
+    })
+
+    if (!configuration){
+        return notFound()
+    }
+
+    const { imageUrl, height, width } = configuration
+
   return (
-    <p>page</p>
+    <DesignConfigurator />
   )
 }
 
