83f356c688bb98312eb6f169c649be7fc6649582
boodovskiy <dzak1987@mail.ru>
2024-11-19 09:32:12 +0200

    Dropdpown styling for Model options

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -12,7 +12,7 @@ import { COLORS, MODELS } from '@/validators/option-validator'
 import { Label } from '@/components/ui/label';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
 import { Button } from '@/components/ui/button';
-import { ChevronsUpDown } from 'lucide-react';
+import { Check, ChevronsUpDown } from 'lucide-react';
 
 interface DesignConfiguratorProps {
     configId: string 
@@ -109,7 +109,12 @@ const DesignConfigurator = ( { configId, imageUrl, imageDimensions }: DesignConf
                                         {MODELS.options.map((model)=> (
                                             <DropdownMenuItem key={model.label} className={cn('flex text-sm gap-1 items-center p-1.5 cursor-default hover:bg-zinc-100', {
                                                 "bg-zinc-100": model.label === options.model.label,
-                                            })}>
+                                            })}
+                                                onClick={() => {
+                                                    setOptions((prev) => ({...prev, model}))
+                                                }}
+                                            >
+                                                <Check className={cn("mr-2 h-4 w-4", model.label === options.model.label ? 'opacity-100' : 'opacity-0')} />
                                                 {model.label}
                                             </DropdownMenuItem>
                                         ))}
