4f7e41cc10c5f89933e37ad534df05032fb912f3
boodovskiy <dzak1987@mail.ru>
2024-11-23 11:42:09 +0200

    Create canvas and convert into png image

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -49,10 +49,51 @@ const DesignConfigurator = ( { configId, imageUrl, imageDimensions }: DesignConf
      async function saveConfiguration() {
         try {
             const { left: caseLeft, top: caseTop, width, height } = phoneCaseRef.current!.getBoundingClientRect()
-            
+
+            const { left: containerLeft, top: containerTop } = containerRef.current!.getBoundingClientRect()
+
+            const leftOffset = caseLeft - containerLeft
+            const topOffset = caseTop - containerTop
+
+            const actualX = renderedPosition.x - leftOffset
+            const actualY = renderedPosition.y - topOffset
+
+            const canvas = document.createElement("canvas")
+            canvas.width = width
+            canvas.height = height
+            const ctx = canvas.getContext('2d')
+
+            const userImage = new Image()
+            userImage.crossOrigin = "anonymous"
+            userImage.src = imageUrl
+            await new Promise((resolve) => (userImage.onload = resolve))
+
+            ctx?.drawImage(
+                userImage,
+                actualX,
+                actualY,
+                renderedDimension.width,
+                renderedDimension.height
+            )
+
+            const base64 = canvas.toDataURL()
+            const base64Data = base64.split(',')[1]
+
+            const blob = base64Blob(base64Data, "image/png")
+            const file = new File([blob], "filename.png", { type: 'image/png' })
         } catch (error) {
             
         }
+     }
+
+     function base64Blob(base64: string, mimeType: string) {
+        const byteCharacters = atob(base64)
+        const byteNumbers = new Array(byteCharacters.length)
+        for(let i = 0; i < byteCharacters.length; i++){
+            byteNumbers[i] = byteCharacters.charCodeAt(i)
+        }
+        const byteArray = new Uint8Array(byteNumbers)
+        return new Blob([byteArray], { type: mimeType })
      }
 
    return (
