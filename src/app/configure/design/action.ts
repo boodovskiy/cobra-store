d43f9ff8d0f0e3dc6c40eeff43485aa0a300723d
boodovskiy <dzak1987@mail.ru>
2024-11-24 19:11:23 +0200

    Add saveConfig on server

--- src/app/configure/design/action.ts
+++ src/app/configure/design/action.ts
@@ -1,38 +1,23 @@
-"use server";
+"use server"
 
-import { db } from "@/db";
-import {
-  CaseColor,
-  CaseFinish,
-  CaseMaterial,
-  PhoneModel,
-} from "@prisma/client";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-
-export type SaveConfigArgs = {
-  color: CaseColor;
-  finish: CaseFinish;
-  material: CaseMaterial;
-  model: PhoneModel;
-  configId: string;
-};
+import { db } from "@/db"
+import { CaseColor, CaseFinish, CaseMaterial, PhoneModel } from "@prisma/client"
 
 export async function saveConfig({
-  color,
-  finish,
-  material,
-  model,
-  configId,
-}: SaveConfigArgs) {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
-
-  if (!user?.id) {
-    throw new Error("You need to be logged in to save configuration");
-  }
-
-  await db.configuration.update({
-    where: { id: configId },
-    data: { color, finish, material, model },
-  });
+    color,
+    finish,
+    material,
+    model,
+    configId,
+}: {
+    color: CaseColor,
+    finish: CaseFinish,
+    material: CaseMaterial,
+    model: PhoneModel,
+    configId: string
+}) {
+    await db.configuration.update({
+        where: {id: configId},
+        data: {color, finish, material, model },
+    })
 }
