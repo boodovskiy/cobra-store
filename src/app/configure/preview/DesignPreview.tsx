f9c06375bb5ad36fe05f9b770d513a601c95d90c
boodovskiy <dzak1987@mail.ru>
2024-11-25 12:48:25 +0200

    Create DesignPreview component

---
+++ src/app/configure/preview/DesignPreview.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const DesignPreview = () => {
+  return (
+    <>
+        <div 
+            aria-hidden='true'
+            className="pointer-events-none select-none absolute inset-0 overflow-hidden flex justify-center">
+                
+         </div>
+    </>
+  )
+}
+
+export default DesignPreview
