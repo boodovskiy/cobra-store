0f36ef3e452604e12491576b505f7aac35f9ae1c
boodovskiy <dzak1987@mail.ru>
2024-11-28 09:40:50 +0200

    Add Order creation function

--- src/app/configure/preview/actions.ts
+++ src/app/configure/preview/actions.ts
@@ -4,6 +4,7 @@ import { PRODUCT_PRICES } from "@/config/products"
 import { db } from "@/db"
 import { BASE_PRICE } from "@/validators/option-validator"
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+import { Order } from '@prisma/client'
 
 export const createCheckoutSession = async ({configId}: {configId: string}) => {
     const configuration = await db.configuration.findUnique({
@@ -22,6 +23,27 @@ export const createCheckoutSession = async ({configId}: {configId: string}) => {
     let price = BASE_PRICE
     if (finish==="textured") price += PRODUCT_PRICES.finish.textured
     if (material==="polycarbonate") price += PRODUCT_PRICES.material.polycarbonate
+
+    let order: Order | undefined = undefined
+
+    const existingOrder = await db.order.findFirst({
+        where: {
+            userId: user.id,
+            configurationId: configuration.id
+        },
+    })
+
+    if (existingOrder){
+        order = existingOrder
+    } else {
+        order = await db.order.create({
+            data: {
+                amount: price / 100,
+                userId: user.id,
+                configurationId: configuration.id
+            },
+        })
+    }
 
     
 }
