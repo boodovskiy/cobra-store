84ae9be7d38d6f17e9af3ba506abe9326e2d89ed
boodovskiy <dzak1987@mail.ru>
2024-11-28 10:03:29 +0200

    Finalize set up StripeSession

--- src/app/configure/preview/actions.ts
+++ src/app/configure/preview/actions.ts
@@ -2,6 +2,7 @@
 
 import { PRODUCT_PRICES } from "@/config/products"
 import { db } from "@/db"
+import { stripe } from "@/lib/stripe"
 import { BASE_PRICE } from "@/validators/option-validator"
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 import { Order } from '@prisma/client'
@@ -45,5 +46,28 @@ export const createCheckoutSession = async ({configId}: {configId: string}) => {
         })
     }
 
-    
+    const product = await stripe.products.create({
+        name: "Custom iPhone case",
+        images: [configuration.imageUrl],
+        default_price_data: {
+            currency: 'USD',
+            unit_amount: price,
+        },
+    })
+
+    const stripeSession = await stripe.checkout.sessions.create({ 
+        success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
+        cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/configure/preview?id=${configuration.id}`,
+        payment_method_types: ["card", "paypal"],
+        mode: "payment",
+        shipping_address_collection: { allowed_countries: ["DE", "UA", "US"] },
+        metadata: {
+            userId: user.id,
+            orderId: order.id,
+        },
+        line_items: [{ price: product.default_price as string, quantity: 1 }],
+
+    })
+
+    return {url: stripeSession.url }
 }
