090a46c8b82ec648b974d5291bb2328bc8edc791
boodovskiy <dzak1987@mail.ru>
2024-11-30 10:54:34 +0200

    Add LoginModal popup before checkout if user not authenticated

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -14,6 +14,7 @@ import { createCheckoutSession } from './actions'
 import { useRouter } from 'next/navigation'
 import { useToast } from '@/hooks/use-toast'
 import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
+import LoginModal from '@/components/LoginModal'
 
 const DesignPreview = ({configuration}: { configuration: Configuration }) => {
     const router = useRouter()
@@ -69,6 +70,8 @@ const DesignPreview = ({configuration}: { configuration: Configuration }) => {
             className="pointer-events-none select-none absolute inset-0 overflow-hidden flex justify-center">
             <Confetti active={ showConfetti } config={{ elementCount:200, spread: 90 }}/>    
          </div>
+         
+         <LoginModal isOpen={isLoginModalOpen} setIsOpen={setIsLoginModalOpen} />
 
          <div className="mt-20 grid grid-cols-1 text-sm sm:grid-cols-12 sm:grid-rows-1 sm:gap-x-6 md:gap-x-8 lg:gap-x-12">
             <div className="sm:col-span-4 md:col-span-3 md:row-span-2 md:row-end-2">
@@ -135,7 +138,7 @@ const DesignPreview = ({configuration}: { configuration: Configuration }) => {
                     </div>
                     
                     <div className="mt-8 flex justify-end pb-12">
-                            <Button onClick={() => createPaymentSession({configId: configuration.id})} className='px-4 sm:px-6 lg:px-8'>
+                            <Button onClick={() => handleCheckout()} className='px-4 sm:px-6 lg:px-8'>
                                 Checkout <ArrowRight className='h-4 w-4 ml-1.5 inline'/>
                             </Button>
                     </div>
