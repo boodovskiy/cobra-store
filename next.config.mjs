b9bcb152070517c8a6d9ceec025d3a420f3e3b6c
boodovskiy <dzak1987@mail.ru>
2024-12-02 10:23:28 +0200

    Fix standalone (new syntax)

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,7 @@ const nextConfig = {
     images: {
         domains: ["utfs.io"]
     },
-    experimental: {
-        outputStandalone: true,
-    },
+    output: 'standalone',
 };
 
 export default nextConfig;
