d0af0ca07ed18a23ab30a55d5f32147edd3774a2
boodovskiy <dzak1987@mail.ru>
2024-12-03 09:57:10 +0200

    Add Thank you page

---
+++ src/app/thank-you/ThankYou.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import { useQuery } from "@tanstack/react-query"
+import { getPaymentStatus } from "./actions"
+import { useSearchParams } from "next/navigation"
+
+const ThankYou = () => {
+    const searchParams = useSearchParams()
+    const orderId = searchParams.get("orderId") || ''
+
+    const { data } = useQuery({
+        queryKey: ["get-payment-status"],
+        queryFn: async () => await getPaymentStatus({ orderId }),
+        retry: true,
+        retryDelay: 500,
+    })
+
+  return (
+    <div>ThankYou</div>
+  )
+}
+
+export default ThankYou
