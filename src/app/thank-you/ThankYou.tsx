f502dc5ac886a32f89879302dc3a87aad6edf0b3
boodovskiy <dzak1987@mail.ru>
2024-12-05 08:47:18 +0200

    Add PhonePreview component for thank-you page

--- src/app/thank-you/ThankYou.tsx
+++ src/app/thank-you/ThankYou.tsx
@@ -4,6 +4,7 @@ import { useQuery } from "@tanstack/react-query"
 import { getPaymentStatus } from "./actions"
 import { useSearchParams } from "next/navigation"
 import { Divide, Loader2 } from "lucide-react"
+import PhonePreview from "@/components/PhonePreview"
 
 const ThankYou = () => {
     const searchParams = useSearchParams()
@@ -69,7 +70,7 @@ const ThankYou = () => {
             </div>
 
             <div className="flex space-x-6 overflow-hidden mt-4 rounded-xl bg-gray-900/5 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl">
-                
+                <PhonePreview color={color!} croppedImageUrl={configuration.croppedImageUrl!}/>
             </div>
         </div>
     </div>
