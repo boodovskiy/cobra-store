4f3d78c82d9b72620e72879d28db7dc666df9ef2
boodovskiy <dzak1987@mail.ru>
2024-12-05 12:16:14 +0200

    Remove test cases from the code

--- src/app/auth-callback/action.ts
+++ src/app/auth-callback/action.ts
@@ -6,8 +6,6 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 export const getAuthStatus = async () => {
     const { getUser } = getKindeServerSession()
     const user = await getUser()
-    console.log("from auth-callback")
-    console.log(user)
 
     if(!user?.id || !user?.email) throw new Error('Invalid user data.')
         
