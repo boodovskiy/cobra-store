e0287b19dde4f2ab5b55a4d50fab0c328946644e
boodovskiy <dzak1987@mail.ru>
2025-12-08 10:19:17 +0200

    fix: patched against CVE-2025-59927.

---
+++ middleware.ts
@@ -0,0 +1,12 @@
+import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
+
+export default withAuth;
+
+export const config = {
+  matcher: [
+    "/dashboard/:path*",
+    "/configure/:path*",
+    "/thank-you/:path*",
+    "/api/uploadthing/:path*",
+  ]
+};
